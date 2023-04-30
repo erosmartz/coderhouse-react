fd6fa6053c9b31031513250e1b2abc7d98cf6f33
erosmartz <107828862+erosmartz@users.noreply.github.com>
2023-04-30 19:41:02 -0300

    Shop working :+1:

--- src/components/ShopCartList/ShopCartList.jsx
+++ src/components/ShopCartList/ShopCartList.jsx
@@ -4,21 +4,25 @@ import { Container, Typography } from "@mui/material";
 import ShopCartListTable from "./ShopCartListTable";
 
 const ShopCartList = () => {
-
   const context = useContext(CartContext);
 
   return (
-    <Container sx={{mt:4, mb:4}}>
+    <Container sx={{ mt: 4, mb: 4 }}>
       <Typography
         align="center"
         variant="button"
-        sx={{ fontSize: "30px", display: "flex", justifyContent: "center",mb:5 }}>
+        sx={{
+          fontSize: "30px",
+          display: "flex",
+          justifyContent: "center",
+          mb: 5,
+        }}>
         Carrito de compras
       </Typography>
 
-      <ShopCartListTable cart={context.cart} setCart={context.setCart}/>
+      <ShopCartListTable cart={context.cart} setCart={context.setCart} />
     </Container>
-  )
-}
+  );
+};
 
-export default ShopCartList
+export default ShopCartList;
--- src/pages/Shop/shop.jsx
+++ src/pages/Shop/shop.jsx
@@ -1,16 +1,38 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+
 import ShopCartList from "../../components/ShopCartList/ShopCartList";
 import ShopForm from "../../components/ShopForm/ShopForm";
 
-import { Container, Divider } from "@mui/material";
+import { Box, Container, Divider, Typography } from "@mui/material";
 
 const Shop = () => {
+  const { cart } = useContext(CartContext);
   return (
     <Container maxWidth="md">
-      <ShopCartList />
-      <Divider />
-      <Container maxWidth="sm">
-        <ShopForm />
-      </Container>
+      {cart.length > 0 ? (
+        <Box>
+          <ShopCartList />
+          <Divider />
+          <Container maxWidth="sm">
+            <ShopForm />
+          </Container>
+        </Box>
+      ) : (
+        <Box sx={{mt:15}}>
+          <Typography
+            align="center"
+            variant="h5"
+            sx={{
+              fontSize: "30px",
+              display: "flex",
+              justifyContent: "center",
+              mb: 5,
+            }}>
+            Tu Carrito de compras está vacío! <br/> {`Agrega algo para ver tu carrito :)`}
+          </Typography>
+        </Box>
+      )}
     </Container>
   );
 };
